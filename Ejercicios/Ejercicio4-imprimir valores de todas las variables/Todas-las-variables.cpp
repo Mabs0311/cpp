a35ebf6e809e3f12b3d64fdce68caefe15142a62
Mabel Matus <andreamabel98@gmail.com>
2022-06-16 15:58:20 -0600

    agregando Todas-las-variables.cpp

---
+++ Ejercicios/Ejercicio4-imprimir valores de todas las variables/Todas-las-variables.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespcae std;
+
+int mainint argc, char const *argv[]()
+{
+
+    bool valorBoolean;
+    valorBoolean = true;
+
+int valorEntero = 15;
+double valorDouble = 20.99;
+string valorString = "Hola como estan";
+char valorChar = 'B';
+
+cout<<"valor Boolean "
+
+
+
+
+
+
+
+
+}
