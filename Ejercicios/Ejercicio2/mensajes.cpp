b4ea06636d6a5fa53c890a52dc870a73e787427a
Mabel Matus <andreamabel98@gmail.com>
2022-06-16 00:33:26 -0600

    main

--- Ejercicios/Ejercicio2/mensajes.cpp
+++ Ejercicios/Ejercicio2/mensajes.cpp
@@ -1,2 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+int main(int argc, char const *argv[])  
+
+{
+
+string nombre;
+
+cout <<"Ingrese su nombre: ";
+cin>>nombre; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
